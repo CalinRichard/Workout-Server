98daf27c6b6b0f305568a1a26b35c468626803aa
George Anton <geocodlea@yahoo.com>
2022-11-13 19:51:47 +0200

    models separation

--- routes/genres.js
+++ routes/genres.js
@@ -1,13 +1,10 @@
-import express from 'express'
-import mongoose, { mongo } from 'mongoose'
-const router = express.Router()
+import express from "express";
+import Movie from "../models/movie.js";
+const router = express.Router();
 
-const schema = new mongoose.Schema({name: 'string', year: 'number', rating: 'number', photo: 'string', genre: 'string'})
-const Genre = mongoose.model('Genre', schema)
+router.get("/", async (req, res) => {
+  const movies = await Movie.find({});
+  res.render("actionMovies", { movies });
+});
 
-router.get('/', async (req, res) => {
-    const movies = await Genre.find({})
-    res.render('actionMovies', {movies})
-  })
-
-  export default router;
+export default router;
